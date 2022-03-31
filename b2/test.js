37a163dcbb572dffc162f064f0187c1239961f26
Angatupyry <fierrofenix@gmail.com>
2022-03-31 07:53:46 -0400

    Cambios enviados con source control

---
+++ b2/test.js
@@ -0,0 +1,22 @@
+const persona = {
+  nombre: "César",
+  apellido: "Rolón",
+  edad: 90,
+  tomaTerere: true,
+};
+
+console.log(persona.nombre);
+console.log(persona.edad);
+
+if (persona.tomaTerere) {
+  console.log("Comprar mucha yerba");
+}
+
+const estudiantes = [
+  {
+    nombre: "César",
+    apellido: "Rolón",
+  },
+  { nombre: "César", apellido: "Rolón" },
+  { nombre: "César", apellido: "Rolón" },
+];
